5549a0ffd88e153279671021aa70650843243a45
Minamoto Xu <nicai@unknow.com>
2019-09-09 13:33:02 +0800

    fix grpc name

--- grpc.go
+++ grpc.go
@@ -5,15 +5,15 @@ import (
 	"google.golang.org/grpc"
 )
 
-// InitGRPCClient InitGRPCClient
+// NewGRPCClient InitGRPCClient
 // svraddr: 服务端地址
 // cafiles: 依次为cert，key，clientca
-func InitGRPCClient(svraddr string, cafiles ...string) (*grpc.ClientConn, error) {
+func NewGRPCClient(svraddr string, cafiles ...string) (*grpc.ClientConn, error) {
 	return microgo.NewGRPCClient(svraddr, cafiles...)
 }
 
-// InitGRPCServer InitGRPCServer
+// NewGRPCServer InitGRPCServer
 // cafiles: 依次为cert，key，clientca
-func InitGRPCServer(cafiles ...string) (*grpc.Server, bool) {
+func NewGRPCServer(cafiles ...string) (*grpc.Server, bool) {
 	return microgo.NewGRPCServer(cafiles...)
 }
