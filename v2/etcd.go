1aadff326415c4fa5a528445e0622e671c4fffb0
Minamoto Xu <nicai@unknow.com>
2021-01-19 10:36:07 +0800

    更新etcd模块，连接失败时会重连

--- v2/etcd.go
+++ v2/etcd.go
@@ -5,7 +5,10 @@ import (
 	"net"
 	"strconv"
 	"strings"
+	"sync"
+	"time"
 
+	"github.com/pkg/errors"
 	"github.com/tidwall/sjson"
 	"github.com/xyzj/gopsu"
 	"github.com/xyzj/gopsu/microgo"
@@ -44,7 +47,7 @@ func (conf *etcdConfigure) show(rootPath string) string {
 }
 
 // NewETCDClient NewETCDClient
-func (fw *WMFrameWorkV2) newETCDClient() bool {
+func (fw *WMFrameWorkV2) newETCDClient() {
 	fw.etcdCtl.addr = fw.wmConf.GetItemDefault("etcd_addr", "127.0.0.1:2378", "etcd服务地址,ip:port格式")
 	fw.etcdCtl.regAddr = fw.wmConf.GetItemDefault("etcd_reg", "", "服务注册地址,ip[:port]格式，不指定port时，自动使用http启动参数的端口")
 	fw.etcdCtl.enable, _ = strconv.ParseBool(fw.wmConf.GetItemDefault("etcd_enable", "true", "是否启用etcd"))
@@ -61,48 +64,60 @@ func (fw *WMFrameWorkV2) newETCDClient() bool {
 	fw.wmConf.Save()
 	fw.etcdCtl.show(fw.rootPath)
 	if !fw.etcdCtl.enable {
-		return false
+		return
 	}
 	if fw.etcdCtl.useauth {
 		fw.etcdCtl.username = "root"
 		fw.etcdCtl.password = gopsu.DecodeString("wMQLEoOHM2eOF6O7Ho8MH74jZ1vMs5i1B+VL+ozl")
 	}
-	var err error
-	if fw.etcdCtl.usetls {
-		fw.etcdCtl.client, err = microgo.NewEtcdv3ClientTLS([]string{fw.etcdCtl.addr}, fw.tlsCert, fw.tlsKey, fw.tlsRoot, fw.etcdCtl.username, fw.etcdCtl.password)
-	} else {
-		fw.etcdCtl.client, err = microgo.NewEtcdv3Client([]string{fw.etcdCtl.addr}, fw.etcdCtl.username, fw.etcdCtl.password)
-	}
-	if err != nil {
-		fw.etcdCtl.enable = false
-		fw.WriteError("ETCD", "Failed connect to "+fw.etcdCtl.addr+"|"+err.Error())
-		return false
-	}
-	fw.etcdCtl.client.SetLogger(&StdLogger{
-		Name:        "ETCD",
-		LogReplacer: strings.NewReplacer("[", "", "]", ""),
-		LogWriter:   fw.wmLog,
-	})
-
-	// 注册自身
 	var httpType = "https"
 	if *debug || *forceHTTP {
 		httpType = "http"
 	}
-	if len(fw.rootPath) > 0 {
-		fw.etcdCtl.client.SetRoot(fw.rootPath)
-	}
-	a, b, err := net.SplitHostPort(fw.etcdCtl.regAddr)
-	if err != nil {
-		a = fw.etcdCtl.regAddr
-	}
-	if b == "" {
-		b = strconv.Itoa(*webPort)
-	}
-	fw.etcdCtl.client.Register(fw.serverName, a, b, httpType, "json")
-	// 获取服务列表信息
-	fw.etcdCtl.client.Watcher()
-	return true
+RUN:
+	var locker sync.WaitGroup
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				fw.WriteError("ETCD", fmt.Sprintf("etcd register crash: %+v", errors.WithStack(err.(error))))
+			}
+			locker.Done()
+		}()
+
+		locker.Add(1)
+		var err error
+		if fw.etcdCtl.usetls {
+			fw.etcdCtl.client, err = microgo.NewEtcdv3ClientTLS([]string{fw.etcdCtl.addr}, fw.tlsCert, fw.tlsKey, fw.tlsRoot, fw.etcdCtl.username, fw.etcdCtl.password)
+		} else {
+			fw.etcdCtl.client, err = microgo.NewEtcdv3Client([]string{fw.etcdCtl.addr}, fw.etcdCtl.username, fw.etcdCtl.password)
+		}
+		if err != nil {
+			fw.etcdCtl.enable = false
+			fw.WriteError("ETCD", "Failed connect to "+fw.etcdCtl.addr+"|"+err.Error())
+			return
+		}
+		fw.etcdCtl.client.SetLogger(&StdLogger{
+			Name:        "ETCD",
+			LogReplacer: strings.NewReplacer("[", "", "]", ""),
+			LogWriter:   fw.wmLog,
+		})
+
+		// 注册自身
+		if len(fw.rootPath) > 0 {
+			fw.etcdCtl.client.SetRoot(fw.rootPath)
+		}
+		a, b, err := net.SplitHostPort(fw.etcdCtl.regAddr)
+		if err != nil {
+			a = fw.etcdCtl.regAddr
+		}
+		if b == "" {
+			b = strconv.Itoa(*webPort)
+		}
+		fw.etcdCtl.client.Register(fw.serverName, a, b, httpType, "json")
+	}()
+	time.Sleep(time.Second * 3)
+	locker.Wait()
+	goto RUN
 }
 
 // ETCDIsReady 返回ETCD可用状态
