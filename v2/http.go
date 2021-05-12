041d09d666df005696b3d488e6e943bcdc0155a8
Minamoto Xu <nicai@unknow.com>
2021-05-12 12:32:52 +0800

    删除_prepareError

--- v2/http.go
+++ v2/http.go
@@ -257,10 +257,10 @@ func (fw *WMFrameWorkV2) PrepareToken(forceAbort ...bool) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		uuid := c.GetHeader("User-Token")
 		if len(uuid) != 36 {
-			c.Params = append(c.Params, gin.Param{
-				Key:   "_prepareError",
-				Value: "User-Token illegal",
-			})
+			// c.Params = append(c.Params, gin.Param{
+			// 	Key:   "_prepareError",
+			// 	Value: "User-Token illegal",
+			// })
 			if shouldAbort {
 				c.Set("status", 0)
 				c.Set("detail", "User-Token illegal")
@@ -271,10 +271,10 @@ func (fw *WMFrameWorkV2) PrepareToken(forceAbort ...bool) gin.HandlerFunc {
 		tokenPath := fw.AppendRootPathRedis("usermanager/legal/" + MD5Worker.Hash([]byte(uuid)))
 		x, err := fw.ReadRedis(tokenPath)
 		if err != nil {
-			c.Params = append(c.Params, gin.Param{
-				Key:   "_prepareError",
-				Value: "User-Token not found",
-			})
+			// c.Params = append(c.Params, gin.Param{
+			// 	Key:   "_prepareError",
+			// 	Value: "User-Token not found",
+			// })
 			if shouldAbort {
 				c.Set("status", 0)
 				c.Set("detail", "User-Token not found")
@@ -284,10 +284,10 @@ func (fw *WMFrameWorkV2) PrepareToken(forceAbort ...bool) gin.HandlerFunc {
 		}
 		ans := gjson.Parse(x)
 		if !ans.Exists() { // token内容异常
-			c.Params = append(c.Params, gin.Param{
-				Key:   "_prepareError",
-				Value: "User-Token can not understand",
-			})
+			// c.Params = append(c.Params, gin.Param{
+			// 	Key:   "_prepareError",
+			// 	Value: "User-Token can not understand",
+			// })
 			if shouldAbort {
 				c.Set("status", 0)
 				c.Set("detail", "User-Token can not understand")
@@ -297,10 +297,10 @@ func (fw *WMFrameWorkV2) PrepareToken(forceAbort ...bool) gin.HandlerFunc {
 			return
 		}
 		if ans.Get("expire").Int() > 0 && ans.Get("expire").Int() < time.Now().Unix() { // 用户过期
-			c.Params = append(c.Params, gin.Param{
-				Key:   "_prepareError",
-				Value: "Account has expired",
-			})
+			// c.Params = append(c.Params, gin.Param{
+			// 	Key:   "_prepareError",
+			// 	Value: "Account has expired",
+			// })
 			if shouldAbort {
 				c.Set("status", 0)
 				c.Set("detail", "Account has expired")
